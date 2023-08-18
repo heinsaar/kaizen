d1b600f17d392d82322a7558d7311dfd0062273c
Leo Heinsaar <leo.heinsaar@hotmail.com>
2023-08-19 01:01:14 +0400

    Test vector::contains()

--- tests/test_containers.h
+++ tests/test_containers.h
@@ -2,9 +2,13 @@
 
 #include "../algorithms.h"
 #include "../masis.h"
+#include <cassert>
 
 void test_vector() {
     mas::vector<int> v;
     mas::populate_random(v);
     mas::print(v);
+
+    v.push_back(777);
+    assert(v.contains(777));
 }
