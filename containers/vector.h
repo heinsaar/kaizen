6df213120b35ac546982f54f0aebdbc809c4fb55
Leo Heinsaar <leo.heinsaar@hotmail.com>
2023-08-18 17:30:14 +0400

    Add vector

---
+++ containers/vector.h
@@ -0,0 +1,28 @@
+#ifndef MASIS_VECTOR_H
+#define MASIS_VECTOR_H
+
+#include <vector>
+
+namespace mas {
+
+template<typename T>
+struct vector : std::vector<T>
+{
+    bool contains(const T& x) const
+    {
+        return std::find(my::begin(), my::end(), x) != my::end();
+    }
+
+    template<typename Pred>
+    bool contains(Pred p) const
+    {
+        return std::find_if(my::begin(), my::end(), p) != my::end();
+    }
+
+private:
+    using my = vector;
+};
+
+} // namespace masis
+
+#endif // MASIS_VECTOR_H
