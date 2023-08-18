b88f1abebbcbaef1dcc6f3e8d9400cda9628bc46
Leo Heinsaar <leo.heinsaar@hotmail.com>
2023-08-18 20:30:26 +0400

    Add populate_random(collection) to algorithms

--- algorithms.h
+++ algorithms.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <iostream>
+#include <random>
 #include <ranges>
 
 namespace mas {
@@ -10,6 +11,19 @@ void print(const C& c)
 {
     for (const auto& x : c)
         std::cout << x << " ";
+}
+
+template<class C>
+void populate_random(C& c)
+{
+    c.resize(11);
+
+    // Seed the random number generator
+    std::random_device              rd;
+    std::mt19937                    gen(rd());
+    std::uniform_int_distribution<> dis(0, 100);
+
+    std::generate(std::begin(c), std::end(c), [&]() { return dis(gen); });
 }
 
 } // namespace mas
