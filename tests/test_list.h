bdab1495b5672151b895c8525381385ed77857d0
Leo Heinsaar <leo.heinsaar@hotmail.com>
2023-08-20 15:52:56 +0400

    Generalize log

--- tests/test_list.h
+++ tests/test_list.h
@@ -7,10 +7,9 @@
 void sanitest_list() {
     zen::list<int> x;
     zen::populate_random(x);
-    zen::print(x);
 
     x.push_back(777);
     assert(x.contains(777));
 
-    std::cout << "TEST PASS: zen::list<T>" << __func__ << std::endl;
+    std::cout << "TEST PASS: " << __func__ << "() CONTENT " << zen::to_string(x) << std::endl;
 }
--- tests/test_vector.h
+++ tests/test_vector.h
@@ -7,10 +7,9 @@
 void sanitest_vector() {
     zen::vector<int> v;
     zen::populate_random(v);
-    zen::print(v);
 
     v.push_back(777);
     assert(v.contains(777));
 
-    std::cout << "TEST PASS: zen::vector<T>" << __func__ << std::endl;
+    std::cout << "TEST PASS: " << __func__ << "() CONTENT " << zen::to_string(v) << std::endl;
 }
