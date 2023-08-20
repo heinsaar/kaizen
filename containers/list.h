7d2e50c0e5e0b99f204b44bc99da3e891c96da4f
Leo Heinsaar <leo.heinsaar@hotmail.com>
2023-08-20 11:28:12 +0400

    Space

--- containers/list.h
+++ containers/list.h
@@ -6,24 +6,24 @@
 
 namespace zen {
 
-    template<class T>
-    struct list : std::list<T>
+template<class T>
+struct list : std::list<T>
+{
+    using std::list<T>::list; // inherit constructors of std::list<T>
+
+    bool contains(const T& x) const
     {
-        using std::list<T>::list; // inherit constructors of std::list<T>
+        return std::find(my::begin(), my::end(), x) != my::end();
+    }
 
-        bool contains(const T& x) const
-        {
-            return std::find(my::begin(), my::end(), x) != my::end();
-        }
+    template<class Pred>
+    bool contains(Pred p) const
+    {
+        return std::find_if(my::begin(), my::end(), p) != my::end();
+    }
 
-        template<class Pred>
-        bool contains(Pred p) const
-        {
-            return std::find_if(my::begin(), my::end(), p) != my::end();
-        }
-
-    private:
-        using my = list;
-    };
+private:
+    using my = list;
+};
 
 } // namespace
