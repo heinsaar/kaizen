4efb6d3e5b76206a436ddba992fe4b751ddf224f
Leo Heinsaar <leo.heinsaar@hotmail.com>
2023-08-27 16:43:43 +0400

    Refactor to modularize make_kaizen.py with functions

--- make_kaizen.py
+++ make_kaizen.py
@@ -2,39 +2,30 @@ import datetime
 import re
 import os
 
-# Dirs to look for header files
-dirs = [
-    '../datas',     # types, data structures, etc.
-    '../functions', # algorithms, utils, etc.
-]
+# Collects header files from specified dirs
+def collect_header_files(dirs):
+    header_files = []
+    for dir in dirs:
+        for filename in os.listdir(dir):
+            if filename.endswith('.h'):
+                header_files.append(os.path.join(dir, filename))
+    return header_files
 
-header_files = []
-
-# Populate header_files dynamically from specified dirs
-for directory in dirs:
-    for filename in os.listdir(directory):
-        if filename.endswith('.h'):
-            header_files.append(os.path.join(directory, filename))
-
-include_directives = set()
-license_text       = []
-code_content       = []
-has_pragma_once    = False
-
-for header_file in header_files:
+# Separates license, include directives and code
+def parse_header_file(header_file):
+    include_directives = set()
+    code_content = []
     with open(header_file, 'r') as input_file:
         lines = input_file.readlines()
-        skipping_license = True  # to skip license comments at the top of files
-        
+        skipping_license = True # to skip license comments at the top of files
         for line in lines:
             if skipping_license:
                 if line.strip().startswith('//'):
-                    continue  # license part, so skip
+                    continue # license part, so skip
                 else:
-                    skipping_license = False  # end of license comment section, prepare for reading code
+                    skipping_license = False # end of license comment section, prepare for reading code
 
             if '#pragma once' in line:
-                has_pragma_once = True
                 continue
 
             match_include = re.match(r'#include\s+["<](.*)[">]', line)
@@ -42,26 +33,41 @@ for header_file in header_files:
                 include_directives.add(line.strip())
             else:
                 code_content.append(line)
+    return include_directives, code_content
 
-# Read license text from LICENSE.txt
-with open('../LICENSE.txt', 'r') as license_file:
-    license_text = license_file.readlines()
-    # Add comment characters if they're not present
-    license_text = ['// ' + line if not line.startswith('// ') else line for line in license_text]
+# Reads license and turns it into a ready comment
+def read_license(filename):
+    with open(filename, 'r') as license_file:
+        lines = license_file.readlines()
+        # Return with comment characters added if they're not present
+        return ['// ' + line if not line.startswith('// ') else line for line in lines]
 
-with open('kaizen.h', 'w') as kaizenh:
-    now = datetime.datetime.now()
-    kaizenh.write('// FILE AUTO-GENERATED ON: ' + now.strftime("%d.%m.%Y %H:%M:%S") + '\n//\n')
-    kaizenh.writelines(license_text)
-    kaizenh.write('\n#pragma once\n\n')
+# Produces the final resulting kaizen library single header file
+def write_output_file(filename, license_text, include_directives, code_content):
+    with open(filename, 'w') as output_file:
+        now = datetime.datetime.now()
+        output_file.write('// FILE AUTO-GENERATED ON: ' + now.strftime("%d.%m.%Y %H:%M:%S") + '\n//\n')
+        output_file.writelines(license_text)
+        output_file.write('\n#pragma once\n\n')
+        for include_directive in sorted(include_directives):
+            output_file.write(include_directive + '\n')
+        # Remove all leading empty lines but one that come right after the #include directives:
+        while code_content and code_content[0].strip() == '':
+            code_content.pop(0)
+        code_content.insert(0, '\n')
+        output_file.writelines(code_content)
+
+if __name__ == '__main__':
+    dirs = ['../datas', '../functions']
+    header_files = collect_header_files(dirs)
+    license_text = read_license('../LICENSE.txt')
+
+    all_include_directives = set()
+    all_code_content = []
+
+    for header_file in header_files:
+        include_directives, code_content = parse_header_file(header_file)
+        all_include_directives.update(include_directives)
+        all_code_content.extend(code_content)
     
-    for include_directive in sorted(include_directives):
-        kaizenh.write(include_directive + '\n')
-
-    # Remove all leading empty lines but one
-    # that come right after the #include directives
-    while code_content and code_content[0].strip() == '':
-        code_content.pop(0)
-    code_content.insert(0, '\n')
-        
-    kaizenh.writelines(code_content)
+    write_output_file('kaizen.h', license_text, all_include_directives, all_code_content)
