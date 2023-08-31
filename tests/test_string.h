d7509f371865731741015db0c8f2582b469e7759
Leo Heinsaar <leo.heinsaar@hotmail.com>
2023-08-31 15:49:40 +0400

    Comment

--- tests/test_string.h
+++ tests/test_string.h
@@ -8,9 +8,10 @@ void sanitest_string()
     std::string s = "[Hello World]";
     zen::string z = s;
 
+    // TODO: Find a better way than asserting
     assert(z.contains("World"));
     assert(s.starts_with("Hello"));
-    assert(s.starts_with("Hello")); // TODO: Find a better way than asserting
+    assert(s.starts_with("Hello"));
 
     zen::log(zen::color::green("TEST PASS:"), __func__);
 }
