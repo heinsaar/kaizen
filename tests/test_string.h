d016997504d450e66d1baae5d86938536441402d
Leo Heinsaar <leo.heinsaar@hotmail.com>
2023-08-31 15:16:40 +0400

    Add zen::string subtest

--- tests/test_string.h
+++ tests/test_string.h
@@ -8,5 +8,7 @@ void sanitest_string()
     std::string s = "Hello World";
     zen::string z = s;
 
+    assert(s.starts_with("Hello")); // TODO: Find a better way than asserting
+
     zen::log(zen::color::green("TEST PASS:"), __func__);
 }
