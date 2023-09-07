18e4de02881c0ca67a81a77a6e98192a813057e2
Leo Heinsaar <leo.heinsaar@hotmail.com>
2023-09-07 17:14:59 +0400

    PR* replace assert with ZEN_EXPECT

--- tests/test_version.h
+++ tests/test_version.h
@@ -6,24 +6,24 @@
 void sanitest_version()
 {
     zen::version v0(1, 2, 3, 4567);
-    assert(v0.major() == 1);
-    assert(v0.minor() == 2);
-    assert(v0.patch() == 3);
-    assert(v0.build() == 4567);
+    ZEN_EXPECT(v0.major() == 1);
+    ZEN_EXPECT(v0.minor() == 2);
+    ZEN_EXPECT(v0.patch() == 3);
+    ZEN_EXPECT(v0.build() == 4567);
 
     zen::version v1("8.2.3.4567");
-    assert(v1.major() == 8);
-    assert(v1.minor() == 2);
-    assert(v1.patch() == 3);
-    assert(v1.build() == 4567);
+    ZEN_EXPECT(v1.major() == 8);
+    ZEN_EXPECT(v1.minor() == 2);
+    ZEN_EXPECT(v1.patch() == 3);
+    ZEN_EXPECT(v1.build() == 4567);
 
-    assert(v0 != v1);
-    assert(v0 < v1);
-    assert(v1 > v0);
+    ZEN_EXPECT(v0 != v1);
+    ZEN_EXPECT(v0 < v1);
+    ZEN_EXPECT(v1 > v0);
 
     using namespace zen::version_literals;
     auto v2 = "7.6.5.4321"_version;
-    assert(v2.build() == 4321);
+    ZEN_EXPECT(v2.build() == 4321);
 
     zen::log(zen::color::green("TEST PASS:"), __func__);
 }
