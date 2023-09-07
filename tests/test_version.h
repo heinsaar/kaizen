bafa6fe4b6b1668d4a5773915ae37af905a769fa
Leo Heinsaar <leo.heinsaar@hotmail.com>
2023-09-07 17:15:47 +0400

    Rhyme

--- tests/test_version.h
+++ tests/test_version.h
@@ -6,20 +6,20 @@
 void sanitest_version()
 {
     zen::version v0(1, 2, 3, 4567);
-    ZEN_EXPECT(v0.major() == 1);
-    ZEN_EXPECT(v0.minor() == 2);
-    ZEN_EXPECT(v0.patch() == 3);
+    ZEN_EXPECT(v0.major() ==    1);
+    ZEN_EXPECT(v0.minor() ==    2);
+    ZEN_EXPECT(v0.patch() ==    3);
     ZEN_EXPECT(v0.build() == 4567);
 
     zen::version v1("8.2.3.4567");
-    ZEN_EXPECT(v1.major() == 8);
-    ZEN_EXPECT(v1.minor() == 2);
-    ZEN_EXPECT(v1.patch() == 3);
+    ZEN_EXPECT(v1.major() ==    8);
+    ZEN_EXPECT(v1.minor() ==    2);
+    ZEN_EXPECT(v1.patch() ==    3);
     ZEN_EXPECT(v1.build() == 4567);
 
     ZEN_EXPECT(v0 != v1);
-    ZEN_EXPECT(v0 < v1);
-    ZEN_EXPECT(v1 > v0);
+    ZEN_EXPECT(v0 <  v1);
+    ZEN_EXPECT(v1 >  v0);
 
     using namespace zen::version_literals;
     auto v2 = "7.6.5.4321"_version;
