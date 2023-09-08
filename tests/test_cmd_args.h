f5c1142a8a4c8d2fc095cf42f4a527ba24aba776
Leo Heinsaar <leo.heinsaar@hotmail.com>
2023-09-08 20:23:32 +0400

    Space

--- tests/test_cmd_args.h
+++ tests/test_cmd_args.h
@@ -7,7 +7,7 @@ void sanitest_cmd_args(int argc, char* argv[])
 {
     zen::log("BEGIN TEST------------------------------------------------", __func__);
     
-    zen::cmd_args       cmd_args(argv, argc);
+    zen::cmd_args        cmd_args(argv, argc);
     const bool verbose = cmd_args.accept("-verbose").is_present();
     const bool absent  = cmd_args.accept("-absent" ).is_present();
 
