cfea715982e247b932d456f86d358e2ce9122e3d
Leo Heinsaar <leo.heinsaar@hotmail.com>
2023-09-09 16:33:24 +0400

    Rename

--- tests/test_version.h
+++ tests/test_version.h
@@ -13,15 +13,15 @@ void sanitest_version()
     ZEN_EXPECT(v0.patch() ==    3);
     ZEN_EXPECT(v0.build() == 4567);
 
-    zen::version v1("8.2.3.4567");
-    ZEN_EXPECT(v1.major() ==    8);
-    ZEN_EXPECT(v1.minor() ==    2);
-    ZEN_EXPECT(v1.patch() ==    3);
-    ZEN_EXPECT(v1.build() == 4567);
+    zen::version v8("8.2.3.4567");
+    ZEN_EXPECT(v8.major() ==    8);
+    ZEN_EXPECT(v8.minor() ==    2);
+    ZEN_EXPECT(v8.patch() ==    3);
+    ZEN_EXPECT(v8.build() == 4567);
 
-    ZEN_EXPECT(v0 != v1);
-    ZEN_EXPECT(v0 <  v1);
-    ZEN_EXPECT(v1 >  v0);
+    ZEN_EXPECT(v0 != v8);
+    ZEN_EXPECT(v0 <  v8);
+    ZEN_EXPECT(v8 >  v0);
 
     using namespace zen::version_literals;
     auto v2 = "7.6.5.4321"_version;
