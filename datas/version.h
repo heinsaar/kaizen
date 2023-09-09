c72438a0cdd3a42379efbc75780218432c73bbee
Leo Heinsaar <leo.heinsaar@hotmail.com>
2023-09-10 02:16:35 +0400

    Reveal more useful information in exception text

--- datas/version.h
+++ datas/version.h
@@ -44,7 +44,10 @@ struct version : std::array<int, 4> {  // read 'struct' as "extend the interface
             at(2) = std::stoi(sm[3]);
             at(3) = std::stoi(sm[4]);
         } else {
-            throw std::invalid_argument{"The given text does not match the version string pattern."};
+            throw std::invalid_argument{
+                // Any cost of typeid is likely to be dwarfed by the cost of the exception anyway
+                std::string(typeid(*this).name()) + " CONSTRUCTOR ARGUMENT STRING DOESN'T MATCH THE EXPECTED M.M.P.B PATTERN."
+            };
         }
     }
 
