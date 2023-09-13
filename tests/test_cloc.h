ffa3ff0c7b880a170da6d3bdb7e86745b41cda8e
Leo <leo.heinsaar@hotmail.com>
2023-09-13 09:57:22 +0400

    PC+ Comment

--- tests/test_cloc.h
+++ tests/test_cloc.h
@@ -8,7 +8,7 @@ void main_test_cloc()
     BEGIN_TEST;
 
     zen::cloc cloc;
-    zen::log(cloc.count({ R"(\.h)" }));
+    zen::log(cloc.count({ R"(\.h)" })); // will pick up kaizen.h
 
     // TODO: Add tests with edge cases. First commit only self-tests this project.
 }
