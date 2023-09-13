300ce666656ee5322b9513a90d0e2c31fa3e623e
Leo <leo.heinsaar@hotmail.com>
2023-09-13 10:40:25 +0400

    Add more cloc tests

--- tests/test_cloc.h
+++ tests/test_cloc.h
@@ -13,7 +13,11 @@ void main_test_cloc()
     zen::cloc clocB(zen::parent_path(), { "build" }); // specify root & subdirs explicitly
     int locB_kaizen_h = clocB.count({ ".h" });        // will pick up kaizen.h
 
+    zen::cloc clocC({ "." });                         // by default, root is current path; specify subdirs (in this case current as ".")
+    int locC_kaizen_h = clocC.count({ ".h" });        // will pick up kaizen.h
+
     ZEN_EXPECT(locA_kaizen_h == locB_kaizen_h);
+    ZEN_EXPECT(locB_kaizen_h == locC_kaizen_h);
 
     // TODO: Add tests with edge cases
 }
