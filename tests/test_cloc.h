f5c6b0a35cbb08c66b981adaf64d09192bb16b72
Leo <leo.heinsaar@hotmail.com>
2023-09-13 10:07:50 +0400

    Add more cloc tests

--- tests/test_cloc.h
+++ tests/test_cloc.h
@@ -7,8 +7,13 @@ void main_test_cloc()
 {
     BEGIN_TEST;
 
-    zen::cloc cloc;
-    zen::log(cloc.count({ R"(\.h)" })); // will pick up kaizen.h
+    zen::cloc clocA;                                  // by default initialized with current dir
+    int locA_kaizen_h = clocA.count({ R"(\.h)" });    // will pick up kaizen.h
+    
+    zen::cloc clocB(zen::parent_path(), { "build" }); // specify root & subdirs explicitly
+    int locB_kaizen_h = clocB.count({ ".h" });        // will pick up kaizen.h
+
+    ZEN_EXPECT(locA_kaizen_h == locB_kaizen_h);
 
     // TODO: Add tests with edge cases. First commit only self-tests this project.
 }
