59628783596f124231e6143ed32e856ba2577152
Leo <leo.heinsaar@hotmail.com>
2023-09-14 09:35:58 +0400

    Rename

--- tests/test_cmd_args.h
+++ tests/test_cmd_args.h
@@ -5,67 +5,67 @@
 void test_cmd_args_empty_args() {
     BEGIN_SUBTEST;
     const char* argv[] = { "exe" };
-    zen::cmd_args cmdargs(argv, 1);
-    ZEN_EXPECT(!cmdargs.is_present("-verbose"));
-    ZEN_EXPECT(!cmdargs.is_present("-ignore"));
+    zen::cmd_args args(argv, 1);
+    ZEN_EXPECT(!args.is_present("-verbose"));
+    ZEN_EXPECT(!args.is_present("-ignore"));
 }
 
 void test_cmd_args_single_arg_present() {
     BEGIN_SUBTEST;
     const char* argv[] = { "exe", "-verbose" };
-    zen::cmd_args cmdargs(argv, 2);
-    cmdargs.accept("-verbose");
-    ZEN_EXPECT(cmdargs.is_present());
+    zen::cmd_args args(argv, 2);
+    args.accept("-verbose");
+    ZEN_EXPECT(args.is_present());
 }
 
 void test_cmd_args_single_arg_not_present() {
     BEGIN_SUBTEST;
     const char* argv[] = { "exe", "-verbose" };
-    zen::cmd_args cmdargs(argv, 2);
-    cmdargs.accept("-ignore");
-    ZEN_EXPECT(!cmdargs.is_present());
+    zen::cmd_args args(argv, 2);
+    args.accept("-ignore");
+    ZEN_EXPECT(!args.is_present());
 }
 
 void test_cmd_args_multiple_args_present() {
     BEGIN_SUBTEST;
     const char* argv[] = { "exe", "-verbose", "-ignore" };
-    zen::cmd_args cmdargs(argv, 3);
-    cmdargs.accept("-verbose").accept("-ignore");
-    ZEN_EXPECT(cmdargs.is_present("-verbose"));
-    ZEN_EXPECT(cmdargs.is_present("-ignore"));
+    zen::cmd_args args(argv, 3);
+    args.accept("-verbose").accept("-ignore");
+    ZEN_EXPECT(args.is_present("-verbose"));
+    ZEN_EXPECT(args.is_present("-ignore"));
 }
 
 void test_cmd_args_multiple_args_one_missing() {
     BEGIN_SUBTEST;
     const char* argv[] = { "exe", "-verbose" };
-    zen::cmd_args cmdargs(argv, 2);
-    cmdargs.accept("-verbose").accept("-ignore");
-    ZEN_EXPECT( cmdargs.is_present("-verbose"));
-    ZEN_EXPECT(!cmdargs.is_present("-ignore"));
+    zen::cmd_args args(argv, 2);
+    args.accept("-verbose").accept("-ignore");
+    ZEN_EXPECT( args.is_present("-verbose"));
+    ZEN_EXPECT(!args.is_present("-ignore"));
 }
 
 void test_cmd_args_arg_at() {
     BEGIN_SUBTEST;
     const char* argv[] = { "exe", "-verbose", "-ignore" };
-    zen::cmd_args cmdargs(argv, 3);
-    ZEN_EXPECT(   cmdargs.arg_at(0) == "exe");
-    ZEN_EXPECT(   cmdargs.arg_at(1) == "-verbose");
-    ZEN_EXPECT(   cmdargs.arg_at(2) == "-ignore");
+    zen::cmd_args args(argv, 3);
+    ZEN_EXPECT(   args.arg_at(0) == "exe");
+    ZEN_EXPECT(   args.arg_at(1) == "-verbose");
+    ZEN_EXPECT(   args.arg_at(2) == "-ignore");
 }
 
 void test_cmd_args_first_last_arg() {
     BEGIN_SUBTEST;
     const char* argv[] = { "exe", "-verbose", "-ignore" };
-    zen::cmd_args cmdargs(argv, 3);
-    ZEN_EXPECT(   cmdargs.first_arg() == "exe");
-    ZEN_EXPECT(   cmdargs.last_arg()  == "-ignore");
+    zen::cmd_args args(argv, 3);
+    ZEN_EXPECT(   args.first_arg() == "exe");
+    ZEN_EXPECT(   args.last_arg()  == "-ignore");
 }
 
 void test_cmd_args_constructor_exceptions() {
     BEGIN_SUBTEST;
     // Test negative argc
     try {
-        zen::cmd_args cmdargs(nullptr, -1);       // expect an exception thrown from ctor
+        zen::cmd_args args(nullptr, -1);       // expect an exception thrown from ctor
         ZEN_EXPECT("SHOULD NOT REACH THIS LINE");
     } catch (const std::invalid_argument& e) {
         ZEN_EXPECT(std::string(e.what()) == "CONSTRUCTOR ARGUMENT argc MUST BE NON-NEGATIVE");
@@ -74,7 +74,7 @@ void test_cmd_args_constructor_exceptions() {
     // Test nullptr in argv
     const char* args_with_null[] = { "valid", nullptr };
     try {
-        zen::cmd_args cmdargs(args_with_null, 2); // expect an exception thrown from ctor
+        zen::cmd_args args(args_with_null, 2); // expect an exception thrown from ctor
         ZEN_EXPECT(false); // Should not reach this line
     } catch (const std::invalid_argument& e) {
         ZEN_EXPECT(std::string(e.what()) == "CONSTRUCTOR ARGUMENT argv CONTAINS nullptr ELEMENT(S)");
@@ -85,13 +85,13 @@ void main_test_cmd_args(int argc, char* argv[])
 {
     BEGIN_TEST;
     
-    zen::cmd_args        cmdargs(argv, argc);
-    const bool verbose = cmdargs.accept("-verbose").is_present();
-    const bool absent  = cmdargs.accept("-absent" ).is_present();
+    zen::cmd_args        args(argv, argc);
+    const bool verbose = args.accept("-verbose").is_present();
+    const bool absent  = args.accept("-absent" ).is_present();
 
-    ZEN_EXPECT( cmdargs.is_present("-verbose"));
-    ZEN_EXPECT(!cmdargs.is_present("-absent"));
-    ZEN_EXPECT(!cmdargs.is_present("-ignore"));
+    ZEN_EXPECT( args.is_present("-verbose"));
+    ZEN_EXPECT(!args.is_present("-absent"));
+    ZEN_EXPECT(!args.is_present("-ignore"));
 
     test_cmd_args_multiple_args_one_missing();
     test_cmd_args_single_arg_not_present();
