2dd175ab1fa609d5e7abacdea2e9b974c2e71438
Leo <leo.heinsaar@hotmail.com>
2023-09-17 23:01:54 +0400

    Add `test_print()` for `zen::print()`

--- tests/test_utils.h
+++ tests/test_utils.h
@@ -78,6 +78,103 @@ void test_to_string()
     ZEN_EXPECT(zen::to_string(vmix, "mixed", 42) == "[1, 2, 3] mixed 42");
 }
 
+void test_print()
+{
+    std::stringstream ss;
+    auto old_buf = std::cout.rdbuf(ss.rdbuf()); // redirect the output
+
+    // Testing zen::print() is a special case since we're testing the same function
+    // that's also used for test output log. Therefor, in order not to pollute the
+    // test output log, we save the standard cout buffer into old_buf at the top of
+    // this function, and restore to it at the bottom - after all the tests in between.
+    // The tests in between juggle around in a way so that ZEN_EXPECT (which uses zen::log(),
+    // which in turn uses zen::print()) behaves as expected instead of outputting into the
+    // intermediate stream used for testing the zen::print(). Tests follow this pattern:
+    // 
+    // 1. Call zen::print() and output into the std::stringstream
+    // 2. Temporarily redirect cout back to the standard output for subsequent ZEN_EXPECT
+    // 3. Use ZEN_EXPECT with the standard output
+    // 4. Redirect cout back to the std::stringstream
+    // 5. Clear the stream with ss.str("")
+
+    // Test with a number
+    zen::print(5);               // print to the stream
+    std::cout.rdbuf(old_buf);
+    ZEN_EXPECT(ss.str() == "5");
+    std::cout.rdbuf(ss.rdbuf());
+    ss.str("");
+
+    // Test with a string
+    zen::print("hello");
+    std::cout.rdbuf(old_buf);
+    ZEN_EXPECT(ss.str() == "hello");
+    std::cout.rdbuf(ss.rdbuf());
+    ss.str("");
+
+    // Test with multiple arguments of mixed types
+    zen::print(5, "hello", 7.2);
+    std::cout.rdbuf(old_buf);
+    ZEN_EXPECT(ss.str() == "5 hello 7.2");
+    std::cout.rdbuf(ss.rdbuf());
+    ss.str("");
+
+    // Test with a vector
+    std::vector<int> v = { 1, 2, 3 };
+    zen::print(v);
+    std::cout.rdbuf(old_buf);
+    ZEN_EXPECT(ss.str() == "[1, 2, 3]");
+    std::cout.rdbuf(ss.rdbuf());
+    ss.str("");
+
+    // Test with nested containers
+    std::vector<std::vector<int>> vv = { {1, 2}, {3, 4} };
+    zen::print(vv);
+    std::cout.rdbuf(old_buf);
+    ZEN_EXPECT(ss.str() == "[[1, 2], [3, 4]]");
+    std::cout.rdbuf(ss.rdbuf());
+    ss.str("");
+
+    // Test with even more nested containers: vector, list, vector
+    std::vector<std::list<std::vector<int>>> vxv = { {{1, 2}, {3, 4}}, {{5, 6}, {7, 8}} };
+    zen::print(vxv);
+    std::cout.rdbuf(old_buf);
+    ZEN_EXPECT(ss.str() == "[[[1, 2], [3, 4]], [[5, 6], [7, 8]]]");
+    std::cout.rdbuf(ss.rdbuf());
+    ss.str("");
+
+    // Test with even more nested containers: list, vector, list
+    std::list<std::vector<std::list<int>>> xvx = { {{1, 2}, {3, 4}}, {{5, 6}, {7, 8}} };
+    zen::print(xvx);
+    std::cout.rdbuf(old_buf);
+    ZEN_EXPECT(ss.str() == "[[[1, 2], [3, 4]], [[5, 6], [7, 8]]]");
+    std::cout.rdbuf(ss.rdbuf());
+    ss.str("");
+
+    // Test with even more nested containers: list, vector, map
+    std::list<std::vector<std::deque<int>>> xvd = { {{1, 2}, {3, 4}}, {{5, 6}, {7, 8}} };
+    zen::print(xvd);
+    std::cout.rdbuf(old_buf);
+    ZEN_EXPECT(ss.str() == "[[[1, 2], [3, 4]], [[5, 6], [7, 8]]]");
+    std::cout.rdbuf(ss.rdbuf());
+    ss.str("");
+
+    // Test with mixed types
+    zen::print("Test", 1, 4.5);
+    std::cout.rdbuf(old_buf);
+    ZEN_EXPECT(ss.str() == "Test 1 4.5");
+    std::cout.rdbuf(ss.rdbuf());
+    ss.str("");
+
+    // Test an empty print call
+    zen::print();
+    std::cout.rdbuf(old_buf);
+    ZEN_EXPECT(ss.str() == "");
+    std::cout.rdbuf(ss.rdbuf());
+    ss.str("");
+
+    std::cout.rdbuf(old_buf); // restore the output
+}
+
 void main_test_utils()
 {
     BEGIN_TEST;
@@ -90,4 +187,5 @@ void main_test_utils()
 
     test_utils_sum();
     test_to_string();
+    test_print();
 }
