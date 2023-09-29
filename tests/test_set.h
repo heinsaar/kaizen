bcc2ccd8a7f74050e66fb6eb108ea0c78cabe6d7
Leo <leo.heinsaar@hotmail.com>
2023-09-29 23:47:18 +0400

    Add `silent_print()` test cases for `zen::set`

--- tests/test_set.h
+++ tests/test_set.h
@@ -2,13 +2,15 @@
 
 #include "kaizen.h" // test using generated header: jump with the parachute you folded
 
+#include "../internal.h"
+
 void test_set_of_strings()
 {
     BEGIN_SUBTEST;
     zen::set<zen::string> x = { "1", "2", "3", "4" };
     x.insert("0");
 
-    //zen::log(typeid(x).name(), x);
+    ZEN_EXPECT(silent_print(x) == "[0, 1, 2, 3, 4]");
 
     ZEN_EXPECT(x.contains("0"));
     ZEN_EXPECT(zen::is_empty(x) == x.is_empty());
@@ -20,7 +22,7 @@ void main_test_multiset()
     zen::multiset<zen::string> x = { "1", "1", "3", "4", "4"};
     x.insert("0");
 
-    // zen::log(x);
+    ZEN_EXPECT(silent_print(x) == "[0, 1, 1, 3, 4, 4]");
 
     ZEN_EXPECT(x.contains("0"));
     ZEN_EXPECT(zen::is_empty(x) == x.is_empty());
@@ -33,7 +35,7 @@ void main_test_set()
     zen::set<int> q = { 1, 2, 3, 4 };
     q.insert(777);
 
-    //zen::log("RANDOM zen::set:", q);
+    ZEN_EXPECT(silent_print(q) == "[1, 2, 3, 4, 777]");
 
     ZEN_EXPECT(q.contains(777));
     ZEN_EXPECT(zen::is_empty(q) == q.is_empty());
