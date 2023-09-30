7d770a3a80b7d1cea7515aa83201e6729021154f
Leo <leo.heinsaar@hotmail.com>
2023-09-30 16:16:01 +0400

    Comment

--- test_kaizen.cpp
+++ test_kaizen.cpp
@@ -52,7 +52,7 @@ int main(int argc, char* argv[])
 
 	zen::timer timer;
 
-	// Since the order of tests doesn't matter, their
+	// Since the order of these tests doesn't matter, their
 	// calls are listed in descending length for aesthetics
 	main_test_cmd_args(argc, argv);
 	main_test_unordered_multiset();
