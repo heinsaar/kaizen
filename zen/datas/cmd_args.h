e12cdb170a0b87437342e6e20f6c317bd90c2bcb
Leo <leo.heinsaar@hotmail.com>
2023-10-04 19:33:43 +0400

    Change return type

--- zen/datas/cmd_args.h
+++ zen/datas/cmd_args.h
@@ -96,7 +96,7 @@ public:
     std::string first_arg() const { return arg_at(0); }
     std::string  last_arg() const { return arg_at(argc_ - 1); }
 
-    int count_accepted() const { return args_accepted_.size(); }
+    std::size_t count_accepted() const { return args_accepted_.size(); }
 
 private:
     using arguments = std::vector<std::string>;
