df6835b01efcbf4ada9854b31d2f862994665ebb
sargishayrapetyan <sargishayrapetyan93@gmail.com>
2023-10-11 11:35:01 +0400

    Add string::is_decimalw + test

--- datas/string.h
+++ datas/string.h
@@ -291,9 +291,10 @@ public:
     bool is_alnum() const { return !is_empty() && std::all_of(my::begin(), my::end(), [](char c) { return std::isalnum(c); }); }
 
     bool is_alpha() const { return !is_empty() && std::all_of(my::begin(), my::end(), [](char c) { return std::isalpha(c); }); }
+
+    bool is_decimal() const { return !is_empty() && std::all_of(my::begin(), my::end(), [](unsigned char c) { return std::isdigit((c)); }); }
     // TODO: Implement all or some of these (from Python string)
     // is_ascii()	    Returns True if all characters in the string are ascii characters
-    // is_decimal()	    Returns True if all characters in the string are decimals
     // is_digit()	    Returns True if all characters in the string are digits
     // is_identifier()	Returns True if the string is an identifier
     // is_lower()	    Returns True if all characters in the string are lower case
