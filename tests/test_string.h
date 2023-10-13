5f7f0281386c46f155a165e277ad2ccc68e544bb
Leo <leo.heinsaar@hotmail.com>
2023-10-13 10:52:04 +0400

    Simplify

--- tests/test_string.h
+++ tests/test_string.h
@@ -911,36 +911,32 @@ void test_string_rpartition()
     BEGIN_SUBTEST;
 
     zen::string s1 = "apple;banana;cherry";
-    zen::string separator1 = ";";
 
-    auto r1 = s1.rpartition(separator1);
+    auto r1 = s1.rpartition(";");
 
     ZEN_EXPECT(std::get<0>(r1) == "apple;banana");
     ZEN_EXPECT(std::get<1>(r1) == ";");
     ZEN_EXPECT(std::get<2>(r1) == "cherry");
 
     zen::string s2 = "one::two::three";
-    zen::string separator2 = "::";
 
-    auto r2 = s2.rpartition(separator2);
+    auto r2 = s2.rpartition("::");
 
     ZEN_EXPECT(std::get<0>(r2) == "one::two");
     ZEN_EXPECT(std::get<1>(r2) == "::");
     ZEN_EXPECT(std::get<2>(r2) == "three");
 
     zen::string s3 = "no_separator_here";
-    zen::string separator3 = "::";
 
-    auto r3 = s3.rpartition(separator3);
+    auto r3 = s3.rpartition("::");
 
     ZEN_EXPECT(std::get<0>(r3) == "no_separator_here");
     ZEN_EXPECT(std::get<1>(r3).empty());
     ZEN_EXPECT(std::get<2>(r3).empty());
 
     zen::string s4 = "::";
-    zen::string separator4 = "::";
 
-    auto r4 = s4.rpartition(separator4);
+    auto r4 = s4.rpartition("::");
 
     ZEN_EXPECT(std::get<0>(r4).empty());
     ZEN_EXPECT(std::get<1>(r4) == "::");
