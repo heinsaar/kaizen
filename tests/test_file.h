092cfbf844ffc43079a715278815ae2c45092eb7
Leo <leo.heinsaar@hotmail.com>
2023-12-19 18:23:47 +0400

    `T` fix

--- tests/test_file.h
+++ tests/test_file.h
@@ -23,14 +23,14 @@ void main_test_file()
     zen::string license = lic.getline(3);
 
     // TODO: Add cases here
-    ZEN_EXPECT(version.contains("1.0.0.0000"));
+    ZEN_EXPECT(version.contains("0.1.0.0000"));
     ZEN_EXPECT(license.contains("MIT"));
 
     zen::string ver = version.extract_version();
 
     zen::version v(ver);
-    ZEN_EXPECT(v.major() ==    1);
-    ZEN_EXPECT(v.minor() ==    0);
+    ZEN_EXPECT(v.major() ==    0);
+    ZEN_EXPECT(v.minor() ==    1);
     ZEN_EXPECT(v.patch() ==    0);
     ZEN_EXPECT(v.build() == 0000);
 
